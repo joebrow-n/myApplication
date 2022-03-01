0bbf16f210950080b1e2afbe7d0fc9b227d82d87
cgill7 <58187215+cgill7@users.noreply.github.com>
2022-03-01 14:19:58 +0000

    test

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace with your ownn action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
