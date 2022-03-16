f7d0e74fac5ff7487dbe3cf4a4a4cea7b0820a51
Joe Brown <brownj8@tcd.ie>
2022-03-16 12:52:27 +0000

    Added a timer to solo doomstop

--- app/src/main/java/com/example/myapplication/solo_doomstop.java
+++ app/src/main/java/com/example/myapplication/solo_doomstop.java
@@ -9,17 +9,42 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.os.CountDownTimer;
 import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
 
 public class solo_doomstop extends Fragment {
+    TextView textView;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         View view = inflater.inflate(R.layout.fragment_solo_doomstop, container, false );
+        textView = (TextView) view.findViewById(R.id.textView13);
+        // Time is in millisecond so 50sec = 50000 I have used
+        // countdown Interveal is 1sec = 1000 I have used
+        new CountDownTimer(86400000, 1000) {
+            public void onTick(long millisUntilFinished) {
+                // Used for formatting digit to be in 2 digits only
+                NumberFormat f = new DecimalFormat("00");
+                long hour = (millisUntilFinished / 3600000) % 24;
+                long min = (millisUntilFinished / 60000) % 60;
+                long sec = (millisUntilFinished / 1000) % 60;
+                textView.setText(f.format(hour) + ":" + f.format(min) + ":" + f.format(sec));
+            }
+            // When the task is over it will print 00:00:00 there
+            public void onFinish() {
+                textView.setText("00:00:00");
+            }
+        }.start();
+
         Button btnToOpenSoloContent = (Button) view.findViewById(R.id.buttonSoloToHome);
         btnToOpenSoloContent.setOnClickListener(new View.OnClickListener() {
             @Override
