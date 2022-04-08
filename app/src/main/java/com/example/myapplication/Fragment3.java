209e48554bfaa10213464b088332f9f581f98445
cgill7 <58187215+cgill7@users.noreply.github.com>
2022-04-08 01:35:40 +0100

    added scoll view to feed page, tried logout(needs working)

--- app/src/main/java/com/example/myapplication/Fragment3.java
+++ app/src/main/java/com/example/myapplication/Fragment3.java
@@ -1,13 +1,29 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class Fragment3 extends Fragment {
     @Nullable
@@ -19,5 +35,13 @@ public class Fragment3 extends Fragment {
 
 
 }
+
+//    public void logout_settingsBTN (View view) {
+//        FirebaseAuth.getInstance().signOut();
+////        startActivity(new Intent(getApplicationContext(),login.class));
+//        startActivity(new Intent(String.valueOf(login.class)));
+////        finish();
+//    }
+
 
 
