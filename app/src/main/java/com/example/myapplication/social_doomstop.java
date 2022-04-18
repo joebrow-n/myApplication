90029729e1a1e6cdfd49563567b18b0bfe2bdb66
Anshkirat Singh Dhanju <97630512+Anshkirat@users.noreply.github.com>
2022-04-18 14:33:35 +0100

    one of the timer approaches tried by Anshkirat and Abhinav.

--- app/src/main/java/com/example/myapplication/social_doomstop.java
+++ app/src/main/java/com/example/myapplication/social_doomstop.java
@@ -17,15 +17,9 @@ import java.text.NumberFormat;
 public class social_doomstop extends Fragment {
 
     TextView textView;
-    TextView Activity;
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_social_doomstop, container, false );
-
-        /* ------ The following is used to implement a timer - needs to be refined ------ */
-        textView = (TextView) view.findViewById(R.id.textView50);
-        Button GenerateSocial = (Button) view.findViewById(R.id.generateSocial);
-        Activity = view.findViewById(R.id.editTextTextMultiLine3soc);
+    @Override
+    public void onCreate (Bundle savedInstanceState)
+    { super.onCreate(savedInstanceState);
         new CountDownTimer(86400000, 1000) {
             public void onTick(long millisUntilFinished) {
                 // Used for formatting digit to be in 2 digits only
@@ -35,11 +29,22 @@ public class social_doomstop extends Fragment {
                 long sec = (millisUntilFinished / 1000) % 60;
                 textView.setText(f.format(hour) + ":" + f.format(min) + ":" + f.format(sec));
             }
+
             // When the task is over it will print 00:00:00 there
             public void onFinish() {
                 textView.setText("00:00:00");
             }
         }.start();
+    }
+
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_social_doomstop, container, false );
+
+        /* ------ The following is used to implement a timer - needs to be refined ------ */
+        textView = (TextView) view.findViewById(R.id.textView50);
+
         /* ------ End of timer ------ */
 
         Button btnToOpenSoloContent = (Button) view.findViewById(R.id.buttonSocialToHome);
@@ -49,13 +54,6 @@ public class social_doomstop extends Fragment {
                 getFragmentManager().beginTransaction().setCustomAnimations(R.anim.enter_bottom_to_top, R.anim.exit_top_to_bottom).detach(social_doomstop.this).commit();
             }
         });
-
-        GenerateSocial.setOnClickListener((new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Activity.setText("PLay Tennis with a partner");
-            }
-        }));
 
         return view;
     }
